158af1ce0b595a4dfdc5518c810a428471188967
Nghia Luong <nghia.luong@nab.com.au>
2020-12-09 14:00:50 +0700

    add more subtring utility

--- utilities/Swift_substring.swift
+++ utilities/Swift_substring.swift
@@ -4,3 +4,12 @@ func subString(_ s: String, start: Int, len: Int) -> String {
     let sub = s[from..<to]
     return String(sub)
 }
+
+extension String {
+    func substring(startIndex: Int, endIndex: Int) -> String {
+        let start = self.index(self.startIndex, offsetBy: startIndex)
+        let end = self.index(self.startIndex, offsetBy: endIndex)
+        let subString = self[start..<end]
+        return String(subString)
+    }
+}
